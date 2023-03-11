682507378a1e809514e99ab7079861ee3d43e4cf
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-03-11 11:27:59 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "dist/asserts"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"]
+                }
+            }
+        },
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+        }]
+    }
+};
